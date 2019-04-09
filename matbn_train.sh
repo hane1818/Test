7bbdef3cc147b5a4200389873e5284c2737aedab
hane <hane0131@gmail.com>
2019-04-09 17:11:37 +0800

    Meow~

---
+++ matbn_train.sh
@@ -0,0 +1,20 @@
+rm -r $1;
+mkdir $1;
+python train.py --pretrained_wordembedding data/CNA_Skip_vectors.txt \
+--pretrained_aux_wordembedding data/CNA_Skip_vectors.txt \
+--preprocessed_data_directory data/preprocessed \
+--gold_summary_directory data/gold-summary \
+--doc_sentence_directory data/Human_TD \
+--data_mode matbn-human \
+--rouge_path /Users/hane/ROUGE-RELEASE-1.5.5 \
+--rouge_data_directory /Users/hane/ROUGE-RELEASE-1.5.5/data \
+--train_epoches 200 \
+--doc_encoder_reverse \
+--attn \
+--rl \
+--num_sample_rollout 5 \
+--train_dir $1; #> $1/train.log;
+#--aux_embedding \
+#--attn \
+#--rl \
+#--bidirectional \
