a081bd47ac6db4afce91e8779cf339476860ea68
hane <hane0131@gmail.com>
2019-04-09 16:57:24 +0800

    Fix error

--- my_args.py
+++ my_args.py
@@ -7,11 +7,11 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--exp_mode', type=str, default='train')
 parser.add_argument('--model_to_load', type=int)
 parser.add_argument('--data_mode', type=str, default='cnn')
-parser.add_argument('--use_gpu', type=bool, default=True)
+parser.add_argument('--use_gpu', action='store_true', default=True)
 
 ## Word level features
 parser.add_argument('--embedding_size', type=int, default=200)
-parser.add_argument('--trainable_wordembed', type=bool, default=False)
+parser.add_argument('--trainable_wordembed', action='store_true', default=False)
 
 ## Sentence level features
 parser.add_argument('--max_sent_length', type=int, default=30)
@@ -29,23 +29,26 @@ parser.add_argument('--max_filter_length', type=int, default=7)
 parser.add_argument('--size', type=int, default=300)
 parser.add_argument('--num_layers', type=int, default=2)
 parser.add_argument('--rnn_cell', type=str, default='gru')
-parser.add_argument('--bidirectional', type=bool, default=False)
+parser.add_argument('--bidirectional', action='store_true', default=False)
 
 ## Encoder Layer features
 # Sentence Encoder
-parser.add_argument('--aux_embedding', type=bool, default=False)
+parser.add_argument('--aux_embedding', action='store_true', default=False)
 
 # Document Encoder
-parser.add_argument('--doc_encoder_reverse', type=bool, default=True)
+parser.add_argument('--doc_encoder_reverse', action='store_true', default=False)
 
 # Extractor
 
+## Attention Mechanism
+parser.add_argument('--attn', action='store_true', default=False)
+
 ## Reinforcement Learning
-parser.add_argument('--rl', type=bool, default=False)
+parser.add_argument('--rl', action='store_true', default=False)
 parser.add_argument('--num_sample_rollout', type=int, default=10)
 
 ## Maximal Marginal Relevance
-parser.add_argument('--mmr', type=bool, default=False)
+parser.add_argument('--mmr', action='store_true', default=False)
 parser.add_argument('--lambda', type=float, default=0.7)
 parser.add_argument('--max_sum_len', type=int, default=10)
 
@@ -59,7 +62,7 @@ parser.add_argument('--train_dir', type=str, default='/address/to/training/direc
 parser.add_argument('--train_epoches', type=int, default=30)
 parser.add_argument('--learning_rate_G', type=float, default=0.001)
 parser.add_argument('--learning_rate_D', type=float, default=0.001)
-parser.add_argument('--weighted_loss', type=bool, default=True)
+parser.add_argument('--weighted_loss', action='store_true')
 parser.add_argument('--batch_size', type=int, default=20)
 parser.add_argument('--training_checkpoint', type=int, default=1)
 parser.add_argument('--summary_ratio', type=float, default=1)
